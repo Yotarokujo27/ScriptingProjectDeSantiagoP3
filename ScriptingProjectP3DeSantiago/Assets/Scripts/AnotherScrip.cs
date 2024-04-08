bf357880a721bdec40e8dea516976cfd78911b1f
Alfredo De Santiago <727601@student.dusd.net>
2024-04-08 10:58:35 -0700

    added a script
    added a new script

---
+++ ScriptingProjectP3DeSantiago/Assets/Scripts/AnotherScrip.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class AnotherScript : MonoBehaviour
+{
+    public int playerScore = 9001;
+}
